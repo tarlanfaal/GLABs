042cb00d6fb0a6415f7329dad8b11c62dc89f4ec
Tarlan Faal <tarlanfaal@yahoo.com>
2024-12-01 23:13:34 -0600

    First commit

---
+++ GLAB 303.5.1 For Loop/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+        public static void main(String[] args) {
+            int sum = 0;
+            int n = 1000;
+            // for loop
+            for (int i = 1; i <= n; ++i) {
+                // body inside for loop
+                sum += i;     // sum = sum + i
+            }
+            System.out.println("Sum = " + sum);
+        }
+    }
